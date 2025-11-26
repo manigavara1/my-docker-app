d6b3b1ce023737678e0decc410da0f1bf681afdc
Your Name <manikrishan751@gmail,com>
2025-11-26 09:37:53 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='', template_folder='')
 
 @app.route("/")
 def home():
-    return "Welcome to Jenkins + Docker Integration!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
